70679b6df8bb9014762cee15bd5e8e5bb224bae6
chinanf-boy <865501259@qq.com>
2018-05-27 14:21:40 +0800

    main is Done

--- index.js
+++ index.js
@@ -1,10 +1,173 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+
+Object.defineProperty(exports, '__esModule', {
+	value: true,
+}); // es6 export default
+const winston = require('winston');
+const Ora = require('ora');
+
+// two-log
+let D = false; // default no debug
+let LOGGER = null; // main
+let LOCK = false; // only one set debug
+let LoggerNAME = 'ora'; // for test
+
+// log
+let defaultWinston = {
+	level: 'debug',
+	transports: [
+		new winston.transports.Console({
+			datePattern: '.yyyy-MM-ddTHH-mm',
+			colorize: true,
+		}),
+		new winston.transports.File({
+			filename: 'translate-info.log',
+			handleExceptions: true,
+			maxsize: 52000,
+			maxFiles: 1,
+			level: 'info',
+			colorize: true,
+		}),
+	],
+};
+let logOpts = null; // winston options merge user options
+
+// two-log
+exports.default = (debug = false, userUse) => {
+	if (typeof debug !== 'boolean') {
+		throw new TypeError(`Expected a string, got ${typeof debug}`);
+	} else {
+		if (LOCK) {
+			throw new TypeError(`Set two-log debug just only one,❌`);
+		}
+
+		D = debug;
+		LoggerNAME = D ? 'log' : 'ora';
+		LOCK = true;
 	}
 
-	opts = opts || {};
+	userUse && userUse(API);
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	function start() {
+		return loggerStart(...arguments);
+	}
+	function text() {
+		return loggerText(...arguments);
+	}
+	function stop() {
+		return loggerStop(...arguments);
+	}
+
+	return {
+		start,
+		text,
+		stop,
+	};
 };
+
+// user use API
+let API = {
+	ora: Ora,
+	log: winston,
+	setLog: options => {
+		logOpts = Object.assign({}, defaultWinston, options);
+	},
+};
+
+// for test
+const _UNLOCK = function() {
+	LOCK = false;
+};
+
+exports.loggerStart = loggerStart;
+exports.loggerText = loggerText;
+exports.loggerStop = loggerStop;
+exports._UNLOCK = _UNLOCK;
+
+/**
+ * @description start logger
+ * @param {string} str
+ * @param {string} options.ora ora color
+ * @param {string} options.log winston log level
+ */
+function loggerStart(str, options = { ora: 'yellow', log: 'log' }) {
+	logOpts = logOpts ? logOpts : defaultWinston;
+
+	let { ora, log } = options;
+	let res = ' '; // for test
+
+	if (!D) {
+		LOGGER = Ora(str).start();
+		LOGGER.color = ora;
+
+		res += 'start'; // for test
+	} else {
+		LOGGER = new winston.Logger(logOpts);
+		LOGGER[log](str);
+
+		res += log; // for test
+	}
+
+	return LoggerNAME + res; // for test
+}
+
+/**
+ * @description set logger text
+ * @param {String} str
+ * @param {string} options.ora ora color
+ * @param {string} options.log winston log level
+ */
+function loggerText(str, options = { ora: 'yellow', log: 'log' }) {
+	if (!LOGGER) {
+		return false;
+	}
+	let { ora, log } = options;
+
+	let res = ' '; // for test
+
+	if (!D) {
+		LOGGER.text = str;
+		LOGGER.color = ora;
+
+		res += 'text'; // for test
+	} else {
+		LOGGER[log](str);
+		res += log; // for test
+	}
+	return LoggerNAME + res; // for test
+}
+
+/**
+ * @description logger stop
+ * @param {string} str
+ * @param {string} options.ora ora {fail|succeed|warn} https://github.com/sindresorhus/ora#instance
+ * @param {string} options.log winston log level
+ */
+function loggerStop(str, options = { ora: '', log: 'log' }) {
+	if (!LOGGER) {
+		return false;
+	}
+	let { ora, log } = options;
+
+	let res = ' '; // for test
+
+	if (!D) {
+		if (ora && str) {
+			LOGGER[ora](str);
+			res += ora; // for test
+		} else {
+			LOGGER.stop();
+			res += 'stop'; // for test
+		}
+	} else {
+		if (str) {
+			LOGGER[log](str);
+		}
+		res += log; // for test
+	}
+
+	LOGGER = null;
+	logOpts = null;
+
+	return LoggerNAME + res; // for test
+}
--- test.js
+++ test.js
@@ -1,11 +1,188 @@
 import test from 'ava';
-import m from '.';
+import log, { loggerStart, loggerText, loggerStop, _UNLOCK } from '.';
 
-test('title', t => {
-	const err = t.throws(() => {
-		m(123);
-	}, TypeError);
-	t.is(err.message, 'Expected a string, got number');
+test.failing('log debug no boolean', t => {
+	let l = log('asdf');
+});
 
-	t.is(m('unicorns'), 'unicorns & rainbows');
+test('ora -- log init', t => {
+	let l = log();
+	t.is(!!l.text, true);
+	t.is(!!l.start, true);
+	t.is(!!l.stop, true);
+});
+
+test.failing('log LOCK , throw', t => {
+	let l = log();
+	t.is(!!l.text, true);
+	t.is(!!l.start, true);
+	t.is(!!l.stop, true);
+});
+
+test('ora not start , return false', t => {
+	_UNLOCK();
+
+	let l = log();
+	t.is(l.text('no start'), false);
+	t.is(l.stop('no start'), false);
+});
+
+test('ora succeed', t => {
+	_UNLOCK();
+	let l = log();
+	let s = 'ora';
+	let oraState = 'succeed';
+	let info = 'info';
+	t.is(l.start('ok ora starting'), s + ' start');
+	t.is(l.text('ok ora running'), s + ' text');
+	t.is(
+		l.stop('ok ora stopping', { ora: oraState, log: info }),
+		s + ' ' + oraState
+	);
+});
+
+test('ora fail', t => {
+	_UNLOCK();
+
+	let l = log();
+	let s = 'ora';
+	let color = 'red';
+	let oraState = 'fail';
+	let info = 'info';
+	t.is(l.start('ok ora starting', { ora: color }), s + ' start');
+
+	t.is(l.text('ok ora running'), s + ' text');
+
+	t.is(
+		l.stop('ok ora stopping', { ora: oraState, log: info }),
+		s + ' ' + oraState
+	);
+});
+
+test('ora stop', t => {
+	_UNLOCK();
+
+	let l = log();
+	let s = 'ora';
+	let oraState = 'success';
+	t.is(l.start('ok ora starting'), s + ' start');
+	t.is(l.text('ok ora running'), s + ' text');
+	t.is(l.stop(), s + ' stop');
+});
+
+test('ora -+= loggerAPI ', t => {
+	let s = 'ora';
+	let oraState = 'succeed';
+	let info = 'info';
+
+	t.is(loggerStart('ora Loggerstart'), s + ' start');
+	t.is(loggerText('ora LoggerText'), s + ' text');
+	t.is(
+		loggerStop('ora LoggerStop', { ora: oraState, log: info }),
+		s + ' ' + oraState
+	);
+});
+
+test('winston -- log default', t => {
+	_UNLOCK();
+
+	let l = log(true);
+	t.is(!!l.text, true);
+	t.is(!!l.start, true);
+	t.is(!!l.stop, true);
+});
+
+test('winston default level:debug  ', t => {
+	_UNLOCK();
+
+	let l = log(true);
+	let s = 'log';
+	let oraState = 'success';
+	let debug = 'debug';
+	let e = 'error';
+	console.log();
+	t.is(
+		l.start('log  default level:debug  starting', { log: debug }),
+		s + ' debug'
+	);
+
+	t.is(l.text('log  default level:debug  running', { log: e }), s + ' error');
+
+	t.is(
+		l.stop('log  default level:debug  stopping', { ora: oraState, log: debug }),
+		s + ' debug'
+	);
+	console.log();
+});
+
+test('winston not start , return false', t => {
+	_UNLOCK();
+
+	let l = log(true);
+	t.is(l.text('no start'), false);
+	t.is(l.stop('no start'), false);
+});
+
+test('winston use API, set winston level:info ', t => {
+	_UNLOCK();
+	let userUse = api => {
+		let winston = api.log;
+		let wopts = {
+			level: 'info',
+		};
+		api.setLog(wopts);
+	};
+	console.log();
+
+	let l = log(true, userUse);
+	let s = 'log';
+	let oraState = 'success';
+	let debug = 'debug';
+	let e = 'error';
+	t.is(
+		l.start('winston level:info winston starting', { log: debug }),
+		s + ' debug'
+	);
+	// return debug , but no console.debug show
+	t.is(l.text('winston level:info winston running', { log: e }), s + ' error');
+
+	t.is(
+		l.stop('winston level:info winston stopping', {
+			ora: oraState,
+			log: debug,
+		}),
+		s + ' debug'
+	);
+
+	console.log();
+});
+
+test('winston -+= loggerAPI ', t => {
+	_UNLOCK();
+
+	let l = log(true);
+	let s = 'log';
+	let oraState = 'success';
+	let debug = 'debug';
+	let e = 'error';
+	console.log();
+
+	t.is(
+		loggerStart('default log:level:debug loggerAPI loggerStart ', {
+			log: debug,
+		}),
+		s + ' debug'
+	);
+	t.is(
+		loggerText('default log:level:debug loggerAPI loggerText ', { log: e }),
+		s + ' error'
+	);
+	t.is(
+		loggerStop('default log:level:debug loggerAPI loggerStop ', {
+			ora: oraState,
+			log: debug,
+		}),
+		s + ' debug'
+	);
+	console.log();
 });
